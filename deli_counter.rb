cf47eb7591774314736daac4ae405dc0f9226b56
Luis Zabala <luis.zabala076@gmail.com>
2019-01-15 15:15:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
 else 
   new_line = "The line is currently:"
-  katz_deli.each.with_index {|customers, index| new_line << " #{index}. #{customers}"}
+  katz_deli.each.with_index {|customers, index| new_line<<" #{index}. #{customers}"}
 end
 end
 puts new_line
